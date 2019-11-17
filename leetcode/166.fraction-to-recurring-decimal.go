e62357d39ea8879cbbba734a13ece41af6b7de14
stokuda <tokuda.sss@gmail.com>
2019-11-18 04:46:57 +0900

    add

--- leetcode/166.fraction-to-recurring-decimal.go
+++ leetcode/166.fraction-to-recurring-decimal.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 /*
  * @lc app=leetcode id=166 lang=golang
  *
@@ -12,68 +7,101 @@ import (
  */
 
 // @lc code=start
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 func fractionToDecimal(numerator int, denominator int) string {
+
 	n := numerator
 	d := denominator
 
+	sign := ""
+	if n*d < 0 {
+		sign = "-"
+	}
+	if n < 0 {
+		n = -1 * n
+	}
+	if d < 0 {
+		d = -1 * d
+	}
+
+	//seen := make([]bool, 15)
+
+	ans := strconv.Itoa(n / d)
 	if n%d == 0 {
-		return strconv.Itoa(n / d)
+		return sign + ans
 	}
 
-	a := float64(n)
-	b := float64(d)
+	ans += "."
+	intMax := 1<<20 - 1
+	qs := make([]int, intMax)
+	rs := make([]int, intMax)
+	var p1, p2, p3, p4 int
+	var q, r int
+	r = (n % d) * 10
+	rs = append(rs, r)
 
-	c := fmt.Sprintf("%.10f", a/b)
+	for i := 0; i < len(qs); i++ {
+		q = r / d
+		qs[i] = q
+		//qs = append(qs, q)
 
-	cLen := len(c)
-	for i := cLen - 1; i >= 0; i-- {
-		if c[i] == '0' {
-			c = c[0:i]
-		} else {
+		r = r % d
+		if r == 0 {
+			return sign + ans + arrayToString(qs[0:i+1])
+		}
+		rs[i] = r
+		r *= 10
+	}
+
+	p1 = 1
+	p2 = 2
+	for p1 < len(qs) {
+		if rs[p1] == rs[p2] {
 			break
 		}
+		p1 = p1 + 1
+		p2 = p2 + 2
 	}
 
-	var l []byte
-	s := 0
-	for i := 0; i < len(c); i++ {
-		if c[i] == '.' {
-			s = i
+	// 循環の開始位置
+	p3 = 0
+	for {
+		if rs[p1] == rs[p3] && qs[p1] == qs[p3] {
 			break
 		}
-		l = append(l, c[i])
+		p1++
+		p3++
 	}
 
-	var r []byte
-	seen := make(map[byte]bool)
-	repeat := false
-	for i := s + 1; i < len(c); i++ {
-		if _, ok := seen[c[i]]; ok {
-
-			var t int
-			for j, v := range r {
-				if v == c[i] {
-					t = j
-				}
-			}
-			var ans_r []byte
-			for j := t; j < len(r); j++ {
-				ans_r = append(ans_r, r[i])
-			}
-			r = append([]byte{'('}, r...)
-
-			repeat = true
+	// 循環の長さ
+	p4 = p3 + 1
+	for {
+		if rs[p3] == rs[p4] {
 			break
 		}
-		r = append(r, c[i])
-		seen[c[i]] = true
+		p4++
 	}
+	fmt.Println(p3, p4, qs, rs)
 
-	if repeat {
-		return string(l) + ".(" + string(r) + ")"
+	for i := 0; i < p3; i++ {
+		ans += strconv.Itoa(qs[i])
 	}
+	ans += "("
+	for i := p3; i < p4; i++ {
+		ans += strconv.Itoa(qs[i])
+	}
+	ans += ")"
+	//ans += arrayToString(qs[0:p3]) + "(" + arrayToString(qs[p3:p4]) + ")"
+	return sign + ans
+}
 
-	return string(l) + "." + string(r)
+func arrayToString(a []int) string {
+	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", "", -1), "[]")
 }
 
 // @lc code=end
